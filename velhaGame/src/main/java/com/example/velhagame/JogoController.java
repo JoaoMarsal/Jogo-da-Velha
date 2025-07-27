98543606d01c25b52fe56b7d8fc16d82dcd3bdf8
JoaoMarsal <98599658+JoaoMarsal@users.noreply.github.com>
2025-07-27 14:30:12 -0400

    decidido o grid
    vou usar grid e tentar usar column constraint

--- velhaGame/src/main/java/com/example/velhagame/JogoController.java
+++ velhaGame/src/main/java/com/example/velhagame/JogoController.java
@@ -1,4 +1,28 @@
 package com.example.velhagame;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.layout.GridPane;
+
+import javax.swing.*;
+
+import static java.lang.Integer.parseInt;
 public class JogoController {
+    @FXML
+    public GridPane tabuleiro;
+    public TextField teste;
+
+    public void change() {
+        int tableN = parseInt(teste.getText());
+        
+
+
+
+    }
 }
--- velhaGame/src/main/java/module-info.java
+++ velhaGame/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.velhagame {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.desktop;
 
 
     opens com.example.velhagame to javafx.fxml;
